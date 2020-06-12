13e65dd818e8ee188b60bea56abe21dda6f28fe7
Jordi Fierro <jordifierromulero@gmail.com>
2020-06-12 17:49:01 +0100

    Convert buzzer.py to a cli script with arguments

--- buzzer.py
+++ buzzer.py
@@ -1,57 +1,54 @@
 import RPi.GPIO as GPIO
 import time
 import math
+import atexit
+import sys
 
-class Buzzer:
+PIN = 7
 
-    PIN = 7
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(PIN, GPIO.OUT)
+    global p
+    p = GPIO.PWM(PIN, 1)
+    p.start(0);
 
-    def setup(self):
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.PIN, GPIO.OUT)
-        self.p = GPIO.PWM(self.PIN, 1)
-        self.p.start(0);
+def tear_down():
+    global p
+    p.ChangeFrequency(1)
+    p.stop()
+    GPIO.output(PIN, GPIO.LOW)
 
-    def tear_down(self):
-        self.p.ChangeFrequency(1)
-        self.p.stop()
-        GPIO.output(self.PIN, GPIO.LOW)
-        GPIO.cleanup()
+def beep(times):
+    while (times > 0):
+        global p
+        p.start(50)
+        p.ChangeFrequency(2000)
+        time.sleep(0.1)
+        p.ChangeFrequency(1)
+        p.stop()
+        times -= 1
+        time.sleep(0.1)
 
-    def run(self, func, *args, **kwargs):
-        self.setup()
-        func(*args, **kwargs)
-        self.tear_down()
-
-    def beep(self, times):
-        self.run(self._beep, times)
-
-    def alarm(self, seconds):
-        self.run(self._alarm, seconds)
-
-    def _beep(self, times):
-        while (times > 0):
-            self.p.start(50)
-            self.p.ChangeFrequency(2000)
-            time.sleep(0.1)
-            self.p.ChangeFrequency(1)
-            self.p.stop()
-            times -= 1
-            time.sleep(0.1)
-
-    def _alarm(self, seconds):
-        t_end = time.time() + seconds
-        while time.time() < t_end:
-            self.p.start(50)
-            for x in range(0, 360):
-                sinVal = math.sin(x * (math.pi / 180.0))
-                toneVal = 2000 + sinVal * 500
-                self.p.ChangeFrequency(toneVal)
-                time.sleep(0.001)
+def alarm(seconds):
+    t_end = time.time() + seconds
+    while time.time() < t_end:
+        global p
+        p.start(50)
+        for x in range(0, 360):
+            sinVal = math.sin(x * (math.pi / 180.0))
+            toneVal = 2000 + sinVal * 500
+            p.ChangeFrequency(toneVal)
+            time.sleep(0.001)
 
 if __name__ == '__main__':
-    buzzer = Buzzer()
-    try:
-        buzzer.beep(1)
-    except KeyboardInterrupt:
-        buzzer.tear_down()
+    atexit.register(tear_down)
+    setup()
+    if (len(sys.argv) != 3):
+        print("Use 'beep times' or 'alarm seconds'")
+    elif (sys.argv[1] == "beep"):
+        beep(int(sys.argv[2]))
+    elif (sys.argv[1] == "alarm"):
+        alarm(int(sys.argv[2]))
+    else:
+        print("Use 'beep times' or 'alarm seconds'")
