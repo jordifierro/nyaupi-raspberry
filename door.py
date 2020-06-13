382b80f87d831a0a359f6c603569c6711f2bacb7
Jordi Fierro <jordifierromulero@gmail.com>
2020-06-13 09:21:20 +0100

    Improve door

--- door.py
+++ door.py
@@ -3,28 +3,20 @@ import time
 
 
 def setup():
-  GPIO.setmode(GPIO.BOARD)
-  GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def loop():
-  while True:
-    if is_open():
-       print("Door is open")
-       time.sleep(2)
-    if not is_open():
-       print("Door is closed")
+def watch():
+    while True:
+       if is_open():
+           print("Door is open")
+       else:
+           print("Door is closed")
        time.sleep(2)
 
 def is_open():
-  return GPIO.input(8)
-
-def destroy():
-  pass
+    return GPIO.input(8)
 
 if __name__ == '__main__':
-  setup()
-  try:
-    loop()
-  except KeyboardInterrupt:
-    GPIO.cleanup()
-    destroy()
+    setup()
+    watch()
