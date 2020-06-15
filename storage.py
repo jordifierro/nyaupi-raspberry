75cd44407b55eb2e3557634a25b3221e3bd48703
Jordi Fierro <jordifierromulero@gmail.com>
2020-06-15 20:29:30 +0100

    Migrate from tinydb to sqlite3

--- storage.py
+++ storage.py
@@ -1,14 +1,33 @@
-from tinydb import TinyDB
+import sqlite3
 
-def table():
-    return TinyDB('db.json').table('status')
+def connection():
+    return sqlite3.connect('nyaupi.db')
 
-def status():
-    return table().all()[0]
+def status_table_exists():
+    c = connection()
+    cursor = c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='STATUS' ''')
+    exists = (cursor.fetchone()[0] == 1)
+    c.commit()
+    c.close()
+    return exists
 
 def initialize_db():
-    table().purge()
-    table().insert({'door_open': False, 'alarm_active': False})
+    c = connection()
+    c.execute('''CREATE TABLE STATUS
+         (ID INT PRIMARY KEY     NOT NULL,
+         ALARM_ACTIVE    INT     NOT NULL,
+         DOOR_OPEN       INT     NOT NULL);''')
+    c.execute("INSERT INTO STATUS (ID,ALARM_ACTIVE,DOOR_OPEN) VALUES (1, 0, 0)");
+    c.commit()
+    c.close()
+
+def status():
+    c = connection()
+    cursor = c.execute("SELECT ID, ALARM_ACTIVE, DOOR_OPEN from STATUS where ID = 1")
+    element = cursor.fetchone()
+    result = {"alarm_active": bool(element[1]), "door_open": bool(element[2])}
+    c.close()
+    return result
 
 def is_door_open():
     return status()['door_open']
@@ -17,13 +36,19 @@ def is_alarm_active():
     return status()['alarm_active']
 
 def set_door_open(is_open):
-    table().update({'door_open': is_open})
+    c = connection()
+    c.execute("UPDATE STATUS set DOOR_OPEN = {} where ID = 1".format(int(is_open)))
+    c.commit()
+    c.close()
 
 def set_alarm_active(is_active):
-    table().update({'alarm_active': is_active})
+    c = connection()
+    c.execute("UPDATE STATUS set ALARM_ACTIVE = {} where ID = 1".format(int(is_active)))
+    c.commit()
+    c.close()
 
 if __name__ == '__main__':
-    if len(table().all()) == 0:
+    if not status_table_exists():
         print('Initializing database...')
         initialize_db()
     print(status())
